969e354ba0440ac0c3b5a62b90a2a17e812220e4
misterpan <misterpancn@gmail.com>
2019-07-23 09:59:41 +0800

    fix cross

--- app/Http/Middleware/EnableCrossRequest.php
+++ app/Http/Middleware/EnableCrossRequest.php
@@ -9,16 +9,16 @@ class EnableCrossRequest
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $response = $next($request);
         $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        $allowOrigin = explode(',', env('ALLOW_ORIGIN')) ?: [];
-        if (in_array($origin, $allowOrigin)) {
+        $allowOrigin = env('ALLOW_ORIGIN') ? explode(',', env('ALLOW_ORIGIN')) : [];
+        if (!empty($origin) && sizeof($allowOrigin) && in_array($origin, $allowOrigin)) {
             $response->header('Access-Control-Allow-Origin', $origin);
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN, Client-Key');
             $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
